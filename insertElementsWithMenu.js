491c0fe6a3f3406edbeed589fe5098ded2b44e1a
Ikuro <nagasawa@lns.tohoku.ac.jp>
2018-02-07 11:44:31 +0900

    FileSystem APIのテスト
    https://www.html5rocks.com/ja/tutorials/file/filesystem/
    を参考にした

--- insertElementsWithMenu.js
+++ insertElementsWithMenu.js
@@ -58,6 +58,40 @@ Draw.loadPlugin(function(ui) {
 		} else {
 		  alert('The File APIs are not fully supported in this browser.');
 		}
+		
+		window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+		function onInitFs(fs) {
+		  console.log('Opened file system: ' + fs.name);
+		}
+		
+		function errorHandler(e) {
+		  var msg = '';
+
+		  switch (e.code) {
+		    case FileError.QUOTA_EXCEEDED_ERR:
+		      msg = 'QUOTA_EXCEEDED_ERR';
+		      break;
+		    case FileError.NOT_FOUND_ERR:
+		      msg = 'NOT_FOUND_ERR';
+		      break;
+		    case FileError.SECURITY_ERR:
+		      msg = 'SECURITY_ERR';
+		      break;
+		    case FileError.INVALID_MODIFICATION_ERR:
+		      msg = 'INVALID_MODIFICATION_ERR';
+		      break;
+		    case FileError.INVALID_STATE_ERR:
+		      msg = 'INVALID_STATE_ERR';
+		      break;
+		    default:
+		      msg = 'Unknown Error';
+		      break;
+		  };
+
+		  console.log('Error: ' + msg);
+		}
+
+		window.requestFileSystem(window.TEMPORARY, 5*1024*1024 /*5MB*/, onInitFs, errorHandler);
     }, null, null, "Ctrl+Shift+E");
     ui.keyHandler.bindAction(69, !0, "test", !0);
     
