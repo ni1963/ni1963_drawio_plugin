36dc175bdb1009055a2c04046afb6e248b647688
Ikuro <nagasawa@lns.tohoku.ac.jp>
2018-02-08 16:22:39 +0900

    FileErrorを削除

--- insertElementsWithMenu.js
+++ insertElementsWithMenu.js
@@ -64,28 +64,7 @@ Draw.loadPlugin(function(ui) {
 		function errorHandler(e) {
 		  var msg = '';
 
-		  switch (e.code) {
-		    case FileError.QUOTA_EXCEEDED_ERR:
-		      msg = 'QUOTA_EXCEEDED_ERR';
-		      break;
-		    case FileError.NOT_FOUND_ERR:
-		      msg = 'NOT_FOUND_ERR';
-		      break;
-		    case FileError.SECURITY_ERR:
-		      msg = 'SECURITY_ERR';
-		      break;
-		    case FileError.INVALID_MODIFICATION_ERR:
-		      msg = 'INVALID_MODIFICATION_ERR';
-		      break;
-		    case FileError.INVALID_STATE_ERR:
-		      msg = 'INVALID_STATE_ERR';
-		      break;
-		    default:
-		      msg = 'Unknown Error';
-		      break;
-		  };
-
-		  console.log('Error: ' + msg);
+		  console.log('Error: ' + e.code);
 		}
 		
 		
